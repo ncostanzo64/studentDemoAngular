1aa3343fa125fd12577bb1c026bd0e4fc4dbd858
Nick <nicholas.costanzo@student.fairfield.edu>
2021-12-08 23:40:39 -0500

    fix

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 //Install express server
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
-var cors = require('cors');
+
 const app = express();
-app.use(cors())
+app.use(cors());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/studentdemoangular'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/studentdemoangular/index.html'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/studentdemoangular/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
