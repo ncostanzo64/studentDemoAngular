622f770f7549ab9cb6f004781c290d22575b7c4f
Nick <nicholas.costanzo@student.fairfield.edu>
2021-12-08 22:58:01 -0500

    Change for AWS

--- src/services/StudentService.ts
+++ src/services/StudentService.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class StudentService {
     findAllStudents = () =>
-        fetch('http://localhost:8080/api/v1/students')
+        fetch('http://service-env.eba-atfpndaa.us-east-2.elasticbeanstalk.com/api/v1/students')
             .then(response => response.json())
 }
